1661002e7d1564973b19d9307dbfc1e798c3468a
Richa Maxx <richamaxx@Richas-MacBook-Air.local>
2023-10-01 00:32:01 -0700

    feat: added functions assignment

--- ex-4.js
+++ ex-4.js
@@ -1,4 +1,13 @@
 // Exercise #4 Grouping Function with Objects
 
 // Start coding here
-let calculator = {};
+let calculator = {
+    add: "add",
+    subtract: "subtract",
+    multiply: "multiply",
+    divide: "divide",
+};
+let resultAdd = add(10, 20)
+console.log(resultAdd);
+let resultDivide = divide(3000, 10);
+console.log(resultDivide);
